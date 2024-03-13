c2c8d4a26dee6a12ba3c7039533676bc44771068
Arhun Saday <arhunsad@gmail.com>
2024-03-13 08:52:59 +0100

    feat(openai api usage): dynamic dates

--- extensions/openai-api-usage/src/index.tsx
+++ extensions/openai-api-usage/src/index.tsx
@@ -22,15 +22,13 @@ interface Preferences {
 
 export default function Command() {
   const preferences = getPreferenceValues<Preferences>();
-  if (preferences.openAiSessionKey === "") {
-    return (
-      <MenuBarExtra title="No session key set" icon={{ source: Icon.Warning, tintColor: Color.Red }} />
-    )
-  }
+  if (preferences.openAiSessionKey === "") { return (<MenuBarExtra title="No session key set" icon={{ source: Icon.Warning, tintColor: Color.Red }} />) }
+
+  const { formattedEndDate, formattedStartDate } = getStartAndEndDates();
 
   const { isLoading, data, revalidate } =
     useFetch<ApiResponse>(
-      'https://api.openai.com/dashboard/billing/usage?end_date=2024-04-01&start_date=2024-03-01',
+      `https://api.openai.com/dashboard/billing/usage?end_date=${formattedEndDate}&start_date=${formattedStartDate}`,
       {
         headers: {
           Authorization: `Bearer ${preferences.openAiSessionKey}`,
@@ -46,4 +44,24 @@ export default function Command() {
       <MenuBarExtra.Item title="Refresh" onAction={revalidate} icon={Icon.RotateClockwise} />
     </MenuBarExtra >
   )
+}
+
+const getStartAndEndDates = () => {
+  const date = new Date();
+  const startYear = date.getFullYear();
+  const startMonth = date.getMonth();
+  const startDate = new Date(startYear, startMonth, 1);
+
+  const endYear = date.getFullYear();
+  const endMonth = date.getMonth();
+  const endDate = new Date(endYear, endMonth + 1, 0);
+
+  // YYYY-MM-DD format
+  const formattedStartDate = `${startYear}-${('0' + (startMonth + 1)).slice(-2)}-${('0' + startDate.getDate()).slice(-2)}`;
+  const formattedEndDate = `${endYear}-${('0' + (endMonth + 1)).slice(-2)}-${('0' + endDate.getDate()).slice(-2)}`;
+
+  return {
+    formattedStartDate,
+    formattedEndDate
+  }
 }
